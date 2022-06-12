1591fc454842f71218ac6032c54385ea478fb879
KlayyMore <99422189+KlayyMore@users.noreply.github.com>
2022-06-12 18:43:23 +0200

    Pages séjours traduite, gourmand et romantique traduites

---
+++ PageSejour.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="Images/eye-mask.png">
+    <title>Venise</title>
+</head>
+
+<body>
+    <?php
+    include("header.php");
+
+    $venise = array("Venise démasquée", "Unmasking Venice");
+    $sousTitre = array("Découverte accompagnée de la ville Lumière", "Guided tour of the City of Lumières");
+    $bienvenue = array("Bienvenue sur la page des séjours", "Welcome to the stays page");
+    $bienvenueTexte = array(
+        "Ici vous retrouverez différents parcours vous accompagnant pour découvrir les différents thèmes de Venise dans vos voyages dans la ville du lion ailé. Bon séjour!",
+        "Here you will find different itineraries to help you discover the different themes of Venice in your travels in the city of the winged lion. Enjoy your stay!"
+    );
+
+    $title = array("Nos parcours", "Our routes");
+
+    $BoutonAller = array("Aller", "Go");
+
+    $GourmandTitre = array("Parcours Gourmand", "The Gourmet Route");
+    $RomantiqueTitre = array("Parcours Romantique", "Romantic Journey");
+    $IncontournablesTitre = array("Parcours des Incontournables", "The Must-See Route");
+
+    $Gourmand = array(
+        "Un parcours de 2 jours permettant de découvrir toutes les spécialités culinaires
+        de Venise à travers divers restaurants.",
+        "A 2-day tour to discover all the culinary specialities of Venice
+        of Venice through various restaurants."
+    );
+
+    $Romantique = array(
+        "Un parcours de 3 jours à faire si vous venez avec votre partenaire afin de découvrir en couple
+        ou lune de miel Venise dans sa splendeur romantique.",
+        "A 3-day tour to do if you come with your partner to discover as a couple or on honeymoon Venice in its romantic splendour.
+        or honeymoon Venice in its romantic splendour."
+    );
+
+    $Incontournables = array(
+        "Un parcours de 4 jours à faire absolument en voyageant à Venise afin de découvrir ses lieux dont vous
+        entendez toujours parler. Afin de pouvoir être celui qui a visité les classiques, ce parcours est là 
+        pour vous.",
+        "A 4-day tour is a must when travelling to Venice to discover the places you always hear about.
+        you always hear about. If you want to be the one who has visited the classics, this itinerary is here 
+        for you."
+    );
+    ?>
+
+
+    <section class="showcase">
+        <video src="videos/backscreen.mov" muted loop autoplay></video>
+        <div class="overlay"></div>
+
+        <div class="container">
+            <div class="text">
+                <?php
+                echo "<h1>$venise[$lang]</h1>";
+                echo "<h2>$sousTitre[$lang]</h2>";
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <div class="bg noir">
+        <article>
+            <?php
+            echo "<h3>$bienvenue[$lang]</h3>";
+            echo "<p> $bienvenueTexte[$lang]</p>"
+            ?>
+        </article>
+    </div>
+
+    <div class="IMGparallax IMG1 IMG-microscopique"></div>
+
+    <div class="bg gris">
+        <article class="centre">
+            <h2><?= "$title[$lang]" ?></h2>
+        </article>
+        <div class="widget bordure-texte">
+            <div class="zone image-avant">
+                <img src="Images/Arancini1.jpg" alt="">
+                <div class="resume">
+                    <?php
+                    echo "<h3>$GourmandTitre[$lang]</h3>";
+                    echo "<p> $Gourmand[$lang]</p>"
+                    ?>
+                </div>
+            </div>
+            <a href="">
+                <div class="voir"><?= "$BoutonAller[$lang]" ?></div>
+            </a>
+        </div>
+    </div>
+
+    <div class="bg gris">
+        <div class="widget bordure-texte">
+            <div class="zone image-apres">
+                <div class="resume">
+                    <?php
+                    echo "<h3>$RomantiqueTitre[$lang]</h3>";
+                    echo "<p> $Romantique[$lang]</p>"
+                    ?>
+                </div>
+                <img src="Images/Arancini1.jpg" alt="">
+            </div>
+            <a href="">
+                <div class="voir"><?= "$BoutonAller[$lang]" ?></div>
+            </a>
+        </div>
+    </div>
+
+    <div class="bg gris">
+        <div class="widget bordure-texte">
+            <div class="zone image-avant">
+                <img src="Images/Arancini1.jpg" alt="">
+                <div class="resume">
+                    <?php
+                    echo "<h3>$IncontournablesTitre[$lang]</h3>";
+                    echo "<p> $Incontournables[$lang]</p>"
+                    ?>
+                </div>
+            </div>
+            <a href="">
+                <div class="voir"><?= "$BoutonAller[$lang]" ?></div>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
